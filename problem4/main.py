58485028ad26a059fa63665480559653d4a1877f
Karno14 <karnopunta29@gmain.com>
2023-10-08 20:38:49 +0700

    menambahkan solusi

--- problem4/main.py
+++ problem4/main.py
@@ -1,6 +1,23 @@
+def prime(start):
+    if start ==1 :
+            return False
+    for i in range (2,start):
+        if start>1 and start%i==0 :
+            return False
+    return True
+
+def prima_setelah_start(start):
+    while not prime(start):
+        start += 1
+    return start
+
 def generate_primes_grid(width, height, start):
-    result = ""
-    return result
+    result = prima_setelah_start(start - 1)  # Menggunakan bilangan prima sebelumnya
+    for i in range(height):
+        for j in range(width):
+            result = prima_setelah_start(result + 1)  # Menggunakan bilangan prima berikutnya
+            print(result, end="\t")
+        print()
 
 if __name__ == "__main__":
     print(generate_primes_grid(2, 3, 13))
