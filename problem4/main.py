93474c17b67f2e077bb8fe325f96fbe137317ddc
Karno14 <karnopunta29@gmain.com>
2023-10-10 12:45:19 +0700

    memperbaiki solusi

--- problem4/main.py
+++ problem4/main.py
@@ -1,23 +1,25 @@
 def prime(start):
-    if start ==1 :
-            return False
-    for i in range (2,start):
-        if start>1 and start%i==0 :
+    if start == 1:
+        return False
+    for i in range(2, start):
+        if start > 1 and start % i == 0:
             return False
     return True
 
-def prima_setelah_start(start):
-    while not prime(start):
-        start += 1
-    return start
-
 def generate_primes_grid(width, height, start):
-    result = prima_setelah_start(start - 1)  # Menggunakan bilangan prima sebelumnya
+    result = ""
+    start += 1
     for i in range(height):
+        row = []
         for j in range(width):
-            result = prima_setelah_start(result + 1)  # Menggunakan bilangan prima berikutnya
-            print(result, end="\t")
-        print()
+            while True:
+                if prime(start):
+                    row.append(start)
+                    start += 1
+                    break
+                start += 1
+        result += " ".join("{:<2}".format(start) for start in row) + "\n"
+    return result
 
 if __name__ == "__main__":
     print(generate_primes_grid(2, 3, 13))
@@ -28,6 +30,6 @@ if __name__ == "__main__":
     """
     print(generate_primes_grid(5, 2, 1))
     """
-    2  3  5  7 11
+    2  3  5  7  11
     13 17 19 23 29
     """
